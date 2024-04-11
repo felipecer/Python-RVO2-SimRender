00ed291cafb24c3854dd0427ebace81a42bd8b91
Felipe Cerda <felipecerdasaavedra@gmail.com>
2024-04-11 00:44:30 -0400

    simulator_core base functionality implemented

--- simulator/simulator_core.py
+++ simulator/simulator_core.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import datetime
 from world_loader import WorldLoader
 
@@ -5,9 +6,8 @@ class SimulationCore:
     def __init__(self, world_loader: WorldLoader, simulation_id: str):
         self.world_loader = world_loader
         self.simulation_id = simulation_id.replace(" ", "_")
-        self.steps_buffer = []
-        self.world_loader.load_world()
-        self.sim = self.world_loader.sim
+        self.steps_buffer = []        
+        _, self.sim, _ = self.world_loader.load_simulation()
 
     def run_simulation(self, steps):
         for step in range(steps):
@@ -24,12 +24,23 @@ class SimulationCore:
             })
         self.steps_buffer.append(step_data)
         
-    def save_simulation_runs(self, filename):
+    def save_simulation_runs(self):
+        timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+        filename = f"{self.world_loader.get_world_name()}_{self.simulation_id}_{timestamp}.txt".replace(" ", "_")
+        
         with open(filename, 'w') as file:
             for step_data in self.steps_buffer:
                 step = step_data['step']
                 for agent_data in step_data['agents']:
                     file.write(f"{step},{agent_data['id']},{agent_data['position'][0]:.2f},{agent_data['position'][1]:.2f}\n")
+        
+        print(f"Archivo de simulación guardado como: {filename}")
     
     def clear_buffer(self):
         self.steps_buffer = []
+
+if __name__ == "__main__":
+    loader = WorldLoader("./worlds/base_scenario.yaml")    
+    sim_core = SimulationCore(loader, "test")   
+    sim_core.run_simulation(500) 
+    sim_core.save_simulation_runs()
