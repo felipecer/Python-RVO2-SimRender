fc29b84474b6afd3d50b9b42033a1e3e7cef2240
Felipe Cerda <75314625+felipecerda-chilesystems@users.noreply.github.com>
2024-05-02 10:49:57 -0400

    gymnasium environment fixed to comply with gymnasium api standard usign stable baselines 3 api checker

--- multi_agent_environment/simple.py
+++ multi_agent_environment/simple.py
@@ -15,15 +15,19 @@ class RVOSimulationEnv(gym.Env):
 
 			self.num_agents = self.sim.getNumAgents()
 			self.action_space = spaces.Box(low=-1.0, high=1.0, shape=(2,), dtype=np.float32)
-			self.observation_space = spaces.Box(low=-np.inf, high=np.inf, 
-																					shape=(4 * (self.num_agents - 1),), dtype=np.float32)
+			self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(4 * (self.num_agents - 1),), dtype=np.float32)
 			# Initialize distances and max time per episode
 			self.initial_distance = np.linalg.norm(np.array(self.sim.getAgentPosition(0)) - np.array(self.agent_goals[0][1]))
 			self.time_limit = 1000  # Set the time limit per episode
 			self.current_step = 0
 	
 	def _get_obs(self):
-		return np.array([self.sim.getAgentPosition(i) for i in range(self.num_agents)])
+		observations = []
+		for i in range(1, self.num_agents):  
+			pos = self.sim.getAgentPosition(i)
+			vel = self.sim.getAgentVelocity(i)
+			observations.extend([pos[0], pos[1], vel[0], vel[1]])
+		return np.array(observations, dtype=np.float32)
 	
 	def _get_info(self):
 		return {}
@@ -35,13 +39,7 @@ class RVOSimulationEnv(gym.Env):
 		self.sim.doStep()
 		self.current_step += 1
 
-		observations = []
-		for agent_id in range(1, self.num_agents):  # Observe other agents
-				pref_vel = self.sim.getAgentPrefVelocity(agent_id)
-				actual_vel = self.sim.getAgentVelocity(agent_id)
-				observations.extend([pref_vel[0], actual_vel[0], pref_vel[1], actual_vel[1]])
-
-		flattened_observations = np.array(observations)
+		flattened_observations = self._get_obs()
 		reward = self.calculate_reward(0)
 		terminated = self.is_done(0)
 		truncated = self.current_step >= self.time_limit
@@ -72,18 +70,19 @@ class RVOSimulationEnv(gym.Env):
 
 	def is_done(self, agent_id):
 		_, goal = self.agent_goals[agent_id]
-		return np.linalg.norm(np.array(self.sim.getAgentPosition(agent_id)) - np.array(goal)) < 0.1
+		distance = np.linalg.norm(np.array(self.sim.getAgentPosition(agent_id)) - np.array(goal))
+		return bool(distance < 0.1)
 
 	def check_collision(self, agent_id):
 		position = self.sim.getAgentPosition(agent_id)
 		radius = self.sim.getAgentRadius(agent_id)
 		for other_id in range(self.num_agents):
-				if other_id != agent_id:
-						other_position = self.sim.getAgentPosition(other_id)
-						other_radius = self.sim.getAgentRadius(other_id)
-						dist = np.sqrt((position[0] - other_position[0]) ** 2 + (position[1] - other_position[1]) ** 2)
-						if dist < (radius + other_radius):
-								return True
+			if other_id != agent_id:
+					other_position = self.sim.getAgentPosition(other_id)
+					other_radius = self.sim.getAgentRadius(other_id)
+					dist = np.sqrt((position[0] - other_position[0]) ** 2 + (position[1] - other_position[1]) ** 2)
+					if dist < (radius + other_radius):
+							return True
 		return False
 
 if __name__ == "__main__":
