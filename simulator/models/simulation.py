a87c1e510e1564be767ffd9eae529c1afab57bac
Felipe Cerda <75314625+felipecerda-chilesystems@users.noreply.github.com>
2024-06-07 12:43:48 -0400

    first test of pydantic models. simulationWorld.yaml added for testing

--- simulator/models/simulation.py
+++ simulator/models/simulation.py
@@ -2,6 +2,8 @@ from typing import List, Optional, Literal, Union
 from pydantic import BaseModel
 from agent import Agent
 from obstacle import Obstacle
+from pydantic import ValidationError
+import yaml
 
 class AgentDefaults(BaseModel):
     neighborDist: float
@@ -16,3 +18,20 @@ class Simulation(BaseModel):
     agentDefaults: AgentDefaults
     agents: List[Agent]
     obstacles: List[Obstacle]
+
+
+def main():
+    # read data from simulationWorld.yaml
+    with open("./simulator/models/simulationWorld.yaml", 'r') as stream:
+        try:
+            data = yaml.safe_load(stream)
+            print(data)
+            simulation = Simulation(**data['Simulation'])
+            print(simulation)
+        except yaml.YAMLError as exc:
+            print(exc)
+        except ValidationError as exc:
+            print(exc)
+
+if __name__ == "__main__":
+    main()
