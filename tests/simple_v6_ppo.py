4214734dde2b96ba3eb67bcfd4c8f983c26cda13
Felipe Cerda <felipecerdasaavedra@gmail.com>
2024-06-16 22:40:16 -0400

    goal spawner class added. simple_v7 added

--- tests/simple_v6_ppo.py
+++ tests/simple_v6_ppo.py
@@ -5,15 +5,15 @@ from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3 import PPO
 
 if __name__ == "__main__":
-    vec_env = make_vec_env(RVOSimulationEnv, n_envs=32, env_kwargs={
-                           "config_file": './simulator/worlds/simple.yaml', "render_mode": None})
-    model = PPO("MlpPolicy", vec_env,  n_steps=2000, verbose=1, device='cpu',
-                tensorboard_log="./tests/logs/ppo_rvo_v6/")
-    # model = PPO.load("ppo_rvo", vec_env, verbose=1, device='cpu')
-    model.learn(total_timesteps=10000000, progress_bar=True)
-    model.save("ppo_rvo_v6")
-    print("Entrenamiento terminado")
-    del model
+    # vec_env = make_vec_env(RVOSimulationEnv, n_envs=32, env_kwargs={
+    #                        "config_file": './simulator/worlds/simple.yaml', "render_mode": None})
+    # model = PPO("MlpPolicy", vec_env,  n_steps=2000, verbose=1, device='cpu',
+    #             tensorboard_log="./tests/logs/ppo_rvo_v6/")
+    # # model = PPO.load("ppo_rvo", vec_env, verbose=1, device='cpu')
+    # model.learn(total_timesteps=10000000, progress_bar=True)
+    # model.save("ppo_rvo_v6")
+    # print("Entrenamiento terminado")
+    # del model
     vec_env = make_vec_env(RVOSimulationEnv, n_envs=4,  env_kwargs={
                            "config_file": './simulator/worlds/simple.yaml', "render_mode": 'rgb'})
     model = PPO.load("ppo_rvo_v6")
