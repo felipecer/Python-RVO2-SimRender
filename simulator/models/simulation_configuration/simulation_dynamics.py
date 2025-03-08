6201c3213e27791ec6a7473c9ad93abe7cba1d23
Felipe Cerda <felipecerdasaavedra@gmail.com>
2025-03-08 16:14:13 -0300

    feat: add stop_on_exit_area dynamic to simulation configurations for various levels

--- simulator/models/simulation_configuration/simulation_dynamics.py
+++ simulator/models/simulation_configuration/simulation_dynamics.py
@@ -202,3 +202,15 @@ class UpdateInitialPositionOnGoalReachedDynamic(EventBasedDynamic):
         current_position = event.current_position
         # Update the initial position in the simulator
         self._simulator.agent_initial_positions[agent_id] = current_position
+
+@register(alias="stop_on_exit_area", category="dynamic")
+class StopOnExitAreaDynamic(OnStepDynamic):
+    boundary_x: float
+    boundary_y: float
+    agent_id: int
+    def execute(self):
+        x, y = self._simulator.get_agent_position(self.agent_id)
+        if not (-self.boundary_x <= x <= self.boundary_x and -self.boundary_y <= y <= self.boundary_y):
+            print(f"Position: {x},{y} is outside the boundary x[-{self.boundary_x}:+{self.boundary_x}], y[-{self.boundary_y}:+{self.boundary_y}]")
+            print(f"Agent {self.agent_id} stepped outside the boundary. Stopping simulation.")
+            self._simulator.stop_simulation()
