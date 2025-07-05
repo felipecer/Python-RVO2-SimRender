9e51458295a9c665902240b096968f175152afe7
D0ggos <83717450+D0ggos@users.noreply.github.com>
2025-07-05 01:56:09 -0400

    Delete simulator directory

--- simulator/models/simulation_configuration/__init__.py
+++
@@ -1,5 +0,0 @@
-# simulator/models/simulation_configuration/__init__.py
-from . import shapes, simulation_dynamics, simulation_events, distribution_patterns
-from .registry import global_registry
-
-from simulator.models.behaviours.ORCA.orca import *
